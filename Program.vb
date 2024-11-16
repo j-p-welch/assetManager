ba1c8c268b9f762f641baa16f6d32b13911e28ea
j-p-welch <housefish17@proton.me>
2024-11-16 10:14:45 -0500

    equipment and part data classes

--- Program.vb
+++ Program.vb
@@ -12,10 +12,12 @@ Public Class LogicalLocation
     <BsonId>
     Public Property LocationID As String
     Public Property Name As String
+    Public Property Subtype As String
     Public Property ParentLocationID As String
     Public Property SubLocationIDs As List(Of String)
 
-    Public Sub New(locationID As String, Optional name As String, Optional parentLocationID As String)
+    Public Sub New(locationID As String, Optional name As String = Nothing, Optional parentLocationID As String = Nothing)
+
         Me.LocationID = locationID
         Me.Name = name
         Me.ParentLocationID = parentLocationID
@@ -23,6 +25,40 @@ Public Class LogicalLocation
 
     End Sub
 
+End Class
 
+Public Class Equipment
+
+    <BsonId>
+    Public Property EquipmentID As String
+    Public Property Name As String
+    Public Property Type As String
+    Public Property Subtype As String
+    Public Property ParentLocationID As String
+    Public Property ParentEquipmentId As String
+    Public Property SubEquipmentIDs As List(Of Equipment)
+    Public Property Parts As List(Of Part)
+
+    Public Sub New(equipmentID As String, Optional name As String = Nothing, Optional type As String = Nothing,
+                   Optional subtype As String = Nothing, Optional parentLocationID As String = Nothing,
+                   Optional parentEquipmentID As String = Nothing)
+
+        Me.EquipmentID = equipmentID
+        Me.Name = name
+        Me.Type = type
+        Me.Subtype = subtype
+        Me.ParentLocationID = parentLocationID
+        Me.ParentEquipmentId = parentEquipmentID
+
+        'need code for adding equipment to parents if not exist
+
+    End Sub
+
+End Class
+
+Public Class Part
+
+    <BsonId>
+    Public Property PartID As String
 
 End Class
